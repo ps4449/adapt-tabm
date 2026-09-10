255f60aad964e6813b2ec70bbadc03589d87c660
priyanka <priyasrini55@gmail.com>
2026-09-11 04:59:32 +0800

    Changes: - Updated model.py to accept configuration for lora_alpha, adapter_scale and lora_input_scaling - Updated LinearLoRAEnsemble in deep.py with the same parameters and added initialization of neural network weight lora_A with uniform dist of inverse square root - Updated summarize_results.py with the same parameters - Added tune_tablora.sh SLURM script to enable finetuning for lr and wd with Optuna - Added sample config 0-tuning.toml to set the finetune configurations

--- paper/bin/model.py
+++ paper/bin/model.py
@@ -120,6 +120,9 @@ class Model(nn.Module):
         ],
         k: None | int = None,
         rank: None | int = None,
+        lora_alpha: None | float = None,
+        adapter_scale: None | float = None,
+        lora_input_scaling: bool = False,
         share_training_batches: bool = DEFAULT_SHARE_TRAINING_BATCHES,
     ) -> None:
         # >>> Validate arguments.
@@ -239,7 +242,15 @@ class Model(nn.Module):
                     lib.deep.LinearLoRAEnsemble,
                     k=k,
                     rank=rank,
+                    lora_alpha=lora_alpha,
+                    adapter_scale=adapter_scale,
                 )
+                if lora_input_scaling:
+                    self.minimal_ensemble_adapter = lib.deep.ScaleEnsemble(
+                        k,
+                        d_flat,
+                        init='random-signs',
+                    )
 
             else:
                 raise ValueError(f'Unknown arch_type: {arch_type}')
@@ -790,8 +801,10 @@ def main(
                 break
             else:
                 assert new_score is not None
+                selected_head = new_idx[-1]
                 greedy_score = new_score
                 greedy_idx = new_idx
+                greedy_mask[selected_head] = True
 
         greedy_heads_output.mkdir(parents=True)
         lib.finish(
